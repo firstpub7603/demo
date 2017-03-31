abfd1eb60c8c68965ba7f5ebcc95a8ec1377e3b8
firstpub <zhouyb@mail.hengfengrunlian.com>
2017-03-31 13:32:33 +0800

    Git测试：第一次创建

---
+++ src/net/z/StrUtil.java
@@ -0,0 +1,29 @@
+/**
+* <p>文件名：StrUtil.java </p>
+* <p>包名：net.z </p>
+* <p>说明：（用一句话简短描述其用途） </p>
+* <p>作者：zyb </p>
+* <p>日期：2017年3月31日 下午1:28:06 </p>
+* <p>版本 V1.0 </p>
+*/
+package net.z;
+
+/**
+* <p>作者: zyb </p>
+* <p>日期: 2017年3月31日 下午1:28:06 </p>
+* <p>说明: （用一句话简短描述其用途） </p> 
+*/
+public class StrUtil {
+	
+
+	/**
+	* <p>作者: zyb </p>
+	* <p>日期: 2017年3月31日 下午1:28:06 </p>
+	* <p>说明: （用一句话简短描述其用途） </p>
+	*/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+	}
+
+}
